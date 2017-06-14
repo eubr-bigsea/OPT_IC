7382d806dd8054409ddc965a79d47d77822d3c3f
Biagio Festa <biagio.festa@gmail.com>
2017-06-14 17:07:44 +0200

    Formatted files helper

--- Helper.hpp
+++ Helper.hpp
@@ -18,43 +18,38 @@ limitations under the License.
 #ifndef _HELPER_HPP_
 #define _HELPER_HPP_
 
-#include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <cstdlib>
 #include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <list>
+#include <sstream>
+#include <string>
+#include <vector>
 
-namespace help
-{
-  typedef unsigned int id_type;
-  typedef long int time_instant;
+namespace help {
+typedef unsigned int id_type;
+typedef long int time_instant;
 
+// read from s dependencies dropping square brackets
+unsigned long read_id(std::string& s);
 
-  //read from s dependencies dropping square brackets
-  unsigned long read_id(std::string& s);
+// create a vector of vectors of string from an input file
+void create_vector(const std::string& file_name,
+                   std::vector<std::vector<std::string> >& v);
 
-  // create a vector of vectors of string from an input file
-  void create_vector(const std::string& file_name, std::vector<std::vector<std::string> >& v);
+// check if string s, including dependencies, is empty
+bool check_empty(const std::string& s);
 
+// adds to the string name the full path of the data directory
+// read from the file config.txt
+void add_data_dir(std::string& name);
 
+// adds to the string name the full path of the lua directory
+// read from the file config.txt
+void add_lua_dir(std::string& name);
 
-  //check if string s, including dependencies, is empty
-  bool check_empty(const std::string& s);
-
-  // adds to the string name the full path of the data directory
-  // read from the file config.txt
-  void add_data_dir(std::string& name);
-
-
-    // adds to the string name the full path of the lua directory
-    // read from the file config.txt
-  void add_lua_dir(std::string& name);
-
-  // prepares the command to call dagsim
-  std::string get_dagsim_command(const std::string& name);
-
+// prepares the command to call dagsim
+std::string get_dagsim_command(const std::string& name);
 }
 #endif
